cf81a44d1092d0325ea8e5cbf693b9d9d8277685
MarcondesJuniorDev <mncjdev@gmail.com>
2024-09-12 11:27:37 -0400

    Inclusão no botão modo escuro nas paǵinas do sistema

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data="{ darkMode: false }"
+    x-bind:class="{'dark' : darkMode === true}" x-init="
+  if (!('darkMode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      localStorage.setItem('darkMode', JSON.stringify(true));
+  }
+  darkMode = JSON.parse(localStorage.getItem('darkMode'));
+  $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
+>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,6 +20,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -32,5 +40,9 @@
                 {{ $slot }}
             </main>
         </div>
+        @stack('modals')
+        @stack('scripts')
+        @livewire('components.dark-mode')
+        @livewireScripts
     </body>
 </html>
